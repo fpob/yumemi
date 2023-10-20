f45f57b30ed4eb606c40d030cc04f519e758e021
Filip Pobořil <tsuki@fpob.cz>
2023-10-20 20:40:34 +0200

    Merge nix/* into flake.nix

--- flake.nix
+++ flake.nix
@@ -26,16 +26,62 @@
 
         formatter = pkgs.nixpkgs-fmt;
 
-        packages =
-          let
-            callPythonPackage = pkgs.lib.callPackageWith (
-              pkgs // pkgs.python3Packages // self.packages.${system}
-            );
-          in
-          rec {
-            default = yumemi;
-            yumemi = callPythonPackage ./nix/yumemi.nix { };
-          };
+        packages = rec {
+          default = yumemi;
+          yumemi =
+            let
+              pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+            in
+            pkgs.python3Packages.buildPythonPackage rec {
+              pname = pyproject.tool.poetry.name;
+              version = pyproject.tool.poetry.version;
+              format = "pyproject";
+
+              src = ./.;
+
+              nativeBuildInputs = [
+                pkgs.installShellFiles
+                pkgs.python3Packages.poetry-core
+                pkgs.python3Packages.pythonRelaxDepsHook
+              ];
+
+              propagatedBuildInputs = [
+                pkgs.python3Packages.attrs
+                pkgs.python3Packages.click
+                pkgs.python3Packages.cryptography
+              ];
+
+              nativeCheckInputs = [
+                pkgs.python3Packages.pytestCheckHook
+                pkgs.python3Packages.pytest-mock
+              ];
+
+              postPatch = ''
+                echo '_LIBNAME="${pkgs.rhash}/lib/librhash.so"' > src/yumemi/_rhash/libname.py
+              '';
+
+              pythonImportsCheck = [
+                "yumemi"
+              ];
+
+              pythonRelaxDeps = [
+                "cryptography"
+              ];
+
+              postInstall = ''
+                installShellCompletion --cmd yumemi \
+                  --bash <(_YUMEMI_COMPLETE=bash_source $out/bin/yumemi) \
+                  --zsh <(_YUMEMI_COMPLETE=zsh_source $out/bin/yumemi) \
+                  --fish <(_YUMEMI_COMPLETE=fish_source $out/bin/yumemi)
+              '';
+
+              meta = {
+                description = pyproject.tool.poetry.description;
+                homepage = pyproject.tool.poetry.repository;
+                license = pkgs.lib.licenses.mit;
+              };
+            };
+        };
 
         checks = {
           yumemi = pkgs.runCommand "yumemi-test"
